145d25269ca5171f47bdbd2b97a9c70be10f5c40
maxminoS <maxminos.1@gmail.com>
2021-04-22 22:53:01 +0700

    Initial commit

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export const App = () => {
+	return (
+		<div></div>
+	)
+}
+
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
+);
