3b33afedb533d2d43f73a3fb6d67e6fed8b7984f
rithikb24 <rithik.b24@gmail.com>
2021-04-24 16:43:56 +0530

    api first commit

---
+++ packages/server/app/main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI
+
+from datetime import datetime
+from typing import Optional
+from typing import List
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi import FastAPI,Request
+from pydantic import BaseModel
+import sentry_sdk
+import sys
+sys.path.append('.')
+from app.utils import get_age
+from fastapi.middleware.cors import CORSMiddleware
+from app.utils.user_validation import UserValidator
+
+class PredictAge(BaseModel):
+    idk_some_input: str
+
+
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/")
+async def root():
+    return "Hello World"
+
+@app.post("/predict_age")
+async def predict_age(input: PredictAge,request:Request):
+    try:
+        get_age(input)
+    except Exception as e:
+
+
+
---
+++ packages/server/app/utils.py
@@ -0,0 +1,5 @@
+from fastapi import Depends, FastAPI, HTTPException, status
+
+def get_age(input):
+
+	return age
