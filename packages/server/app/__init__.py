b37f571bce4520c17c8e8a3c09ebbdc5645c8bf7
maxminoS <maxminos.1@gmail.com>
2021-05-19 02:35:19 +0700

    Check if model exists before running server

--- packages/server/app/__init__.py
+++ packages/server/app/__init__.py
@@ -1,11 +1,20 @@
+import os
 import sys
 sys.path.append(".")
 
+import tensorflow as tf
 from typing import List
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from app.utils import get_age
+
+# Check if model exists
+MODEL = "app/age_model.h5"
+if not os.path.isfile(MODEL):
+	sys.exit("Error: You must run `build_model.py` first")
+else:
+	model = tf.keras.models.load_model(MODEL)
 
 class PredictAge(BaseModel):
 	image_list: List[int]
